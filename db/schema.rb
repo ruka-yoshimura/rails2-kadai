23b87da9e003283ee9db87272ab41f9741ab0b8d
吉村月楓 <yoshimuraruka@MacBook-Air.local>
2024-01-04 11:58:50 +0900

    マイグレーションファイルを修正

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2023_12_31_143158) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "img"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
